905d8ef96eaad3a5fe0487aab2d1817c1d29a84a
Rigo-Dev <juandiaz031317@gmail.com>
2022-10-14 01:44:19 -0300

    Hice curso de lading page

---
+++ js/practice.js
@@ -0,0 +1,17 @@
+/*var color = {
+    editar: prompt("Digite el color a cambiar"),
+    proceso: function(){
+        if(color.editar ==='azul'){
+            this.editar='negro';
+        }else if(color.editar==='negro'){
+            this.editar='azul';
+        }
+    }
+};
+color.proceso();
+console.log(color.editar);    */
+
+
+
+/*Nueva pagina*/
+
